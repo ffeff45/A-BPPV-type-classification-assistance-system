01b58cdc46288257723337473a5f21ebc3d28c72
ffeff <acvo2703@gmail.com>
2024-06-25 22:11:24 +0900

    2024-05-20, Create a video player and apply UI

--- UI/WindowClass1.py
+++ UI/WindowClass1.py
@@ -3,12 +3,14 @@ import sys
 import cv2
 from PyQt5 import uic  # ui 파일을 사용하기 위한 모듈
 from PyQt5.QtWidgets import *
-from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
-from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette
 from WindowClass2 import WindowClass2
+from media import CMultiMedia
 
 # UI파일 연결 코드
 UI_class = uic.loadUiType("SC1.ui")[0]
+
 class WindowClass1(QMainWindow, UI_class):
     def __init__(self) :
         super().__init__()
@@ -17,26 +19,35 @@ class WindowClass1(QMainWindow, UI_class):
 
         self.Sc1_Result.clicked.connect(self.showSecondWindow)
         self.Sc1_Add.clicked.connect(self.pushAddClicked)
+        self.Sc1_Play.clicked.connect(self.clickPlay)
+        self.Sc1_Stop.clicked.connect(self.clickStop)
+        self.Sc1_Pause.clicked.connect(self.clickPause)
+
+        self.Sc1_ListView.itemDoubleClicked.connect(self.dbClickList)
 
         #동영상
+        self.mp = CMultiMedia(self, self.Sc1_Video)
+
+        pal = QPalette()        
+        pal.setColor(QPalette.Background, Qt.black)
+        self.Sc1_Video.setAutoFillBackground(True)
+        self.Sc1_Video.setPalette(pal)
 
 
         # QTableWidget 초기화
         self.Sc1_ListView.setColumnCount(3)
         self.Sc1_ListView.setHorizontalHeaderLabels(['파일명', '프레임 수', '길이'])
 
-        # add버튼
-        self.add_count = 0
 
     def showSecondWindow(self):   # 페이지 바꾸는 코드
         self.hide()
         self.second_window = WindowClass2()
         self.second_window.exec()
         self.show()
+    
 
     # 파일선택버튼
     def pushAddClicked(self):
-
         self.fname = QFileDialog.getOpenFileName(self, '파일선택', '', 'All Files(*.*)')
         if self.fname[0]:  # 파일이 선택된 경우
             filename = os.path.basename(self.fname[0])
@@ -45,17 +56,34 @@ class WindowClass1(QMainWindow, UI_class):
             fps = cap.get(cv2.CAP_PROP_FPS)
             duration = frame_count / fps if fps > 0 else 0  # 영상 길이 계산
 
-
-
             # 리스트에 새로운 항목 추가
-            self.Sc1_ListView.setRowCount(self.add_count)
-            row_position = 0
-            self.Sc1_ListView.insertRow(row_position)
-            self.Sc1_ListView.setItem(row_position, 0, QTableWidgetItem(filename))
-            self.Sc1_ListView.setItem(row_position, 1, QTableWidgetItem(str(frame_count)))
-            self.Sc1_ListView.setItem(row_position, 2, QTableWidgetItem(f"{duration:.2f} 초"))
+            row = self.Sc1_ListView.rowCount()
+            self.Sc1_ListView.setRowCount(row)
+            self.Sc1_ListView.insertRow(row)
+            self.Sc1_ListView.setItem(row, 0, QTableWidgetItem(filename))
+            self.Sc1_ListView.setItem(row, 1, QTableWidgetItem(str(frame_count)))
+            self.Sc1_ListView.setItem(row, 2, QTableWidgetItem(f"{duration:.2f} 초"))
             self.Sc1_ListView.resizeColumnsToContents()
-            self.add_count +=1
+
+            #동영상 
+            self.mp.addMedia(self.fname[0])
+    
+    def updateState(self, msg):
+        self.Sc1_StatusText.setText(msg)
+
+    def clickPlay(self):
+        index = self.Sc1_ListView.rowCount() -1     
+        self.mp.playMedia(index)
+    
+    def clickStop(self):
+        self.mp.stopMedia()
+
+    def clickPause(self):
+        self.mp.pauseMedia()
+    
+    def dbClickList(self, item):
+        row_1 = self.Sc1_ListView.row(item)
+        self.mp.playMedia(row_1)
 
 
 if __name__ == "__main__":
