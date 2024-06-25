f6b2f792b55e4c5556d869b133d31a04ab4c526e
ffeff <acvo2703@gmail.com>
2024-06-25 22:04:23 +0900

    2024-05-20, Adding Table Widgets and Adding Items to Table Widgets

--- UI/WindowClass1.py
+++ UI/WindowClass1.py
@@ -1,11 +1,13 @@
+import os
 import sys
 import cv2
-import os
+from PyQt5 import uic  # ui 파일을 사용하기 위한 모듈
 from PyQt5.QtWidgets import *
-from PyQt5 import uic   # ui 파일을 사용하기 위한 모듈 import
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 from WindowClass2 import WindowClass2
 
-#UI파일 연결 코드
+# UI파일 연결 코드
 UI_class = uic.loadUiType("SC1.ui")[0]
 class WindowClass1(QMainWindow, UI_class):
     def __init__(self) :
@@ -16,33 +18,44 @@ class WindowClass1(QMainWindow, UI_class):
         self.Sc1_Result.clicked.connect(self.showSecondWindow)
         self.Sc1_Add.clicked.connect(self.pushAddClicked)
 
-    def showSecondWindow(self):
-        # 두 번째 윈도우를 생성하고 보여줍니다.
+        #동영상
+
+
+        # QTableWidget 초기화
+        self.Sc1_ListView.setColumnCount(3)
+        self.Sc1_ListView.setHorizontalHeaderLabels(['파일명', '프레임 수', '길이'])
+
+        # add버튼
+        self.add_count = 0
+
+    def showSecondWindow(self):   # 페이지 바꾸는 코드
         self.hide()
         self.second_window = WindowClass2()
         self.second_window.exec()
-        self.show()  # 현재 윈도우를 숨깁니다.
+        self.show()
 
-#파일선택버튼
-
-
+    # 파일선택버튼
     def pushAddClicked(self):
-        self.fname = QFileDialog.getOpenFileName(self, '파일선택','','All Files(*.*)')
-        filename = os.path.basename(self.fname[0])
-        cap = cv2.VideoCapture(self.fname[0])
-        filesize = float(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        print(filesize)
-        self.Sc1_ListView.setItem(0, 0, QTableWidgetItem(filename))
-        self.Sc1_ListView.setItem(0, 1, QTableWidgetItem(str(filesize)))
-        self.Sc1_ListView.resizeColumnsToContents()
+
+        self.fname = QFileDialog.getOpenFileName(self, '파일선택', '', 'All Files(*.*)')
+        if self.fname[0]:  # 파일이 선택된 경우
+            filename = os.path.basename(self.fname[0])
+            cap = cv2.VideoCapture(self.fname[0])
+            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            duration = frame_count / fps if fps > 0 else 0  # 영상 길이 계산
 
 
 
-
-
-
-
-
+            # 리스트에 새로운 항목 추가
+            self.Sc1_ListView.setRowCount(self.add_count)
+            row_position = 0
+            self.Sc1_ListView.insertRow(row_position)
+            self.Sc1_ListView.setItem(row_position, 0, QTableWidgetItem(filename))
+            self.Sc1_ListView.setItem(row_position, 1, QTableWidgetItem(str(frame_count)))
+            self.Sc1_ListView.setItem(row_position, 2, QTableWidgetItem(f"{duration:.2f} 초"))
+            self.Sc1_ListView.resizeColumnsToContents()
+            self.add_count +=1
 
 
 if __name__ == "__main__":
